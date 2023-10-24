968d27320ff833b043633952daf1c3762a2dfed9
LordFabinho <145404540+LordFabinho@users.noreply.github.com>
2023-10-24 20:50:26 -0300

    Add files via upload

---
+++ Automação.py
@@ -0,0 +1,13 @@
+import pandas as pd #iniciar - módulos usados
+resultados = pd.read_csv('Resultados simulado.csv') # ( varivel pd.read_(tipo do arquivo(ex:_csv))('arquivo com '.csv' no final)) #essa função read vai ler o arquivo e o transformar em tabelas 
+print(resultados)
+#abrir site para coleta de dados
+#integrar com o arquivo exel
+#copiar questoes
+#colar questoes na planilha
+#copiar gabarito
+#colar gabarito na planilha
+#copiar ano
+#colar ano na planilha
+#finalizar coleta de questoes
+#colocar na plataforma
---
+++ mathplotlib_resumo.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#Para fazer um gráfico simples, usamos a função "plot". Ela recebe um número arbitrário de argumentos. O mais comum é usá-la da seguinte forma: plt.plot(x, y, formato)
+
+#Aqui, x é uma sequência de COORDENADAS HORIZONTAIS: pode ser uma tupla, uma lista ou um array unidimensional. Analogamente, y é uma sequência de COORDENADAS VERTICAIS. (As sequências devem ter o mesmo tamanho)
+
+#O parâmetro 'FORMATO' é uma string especificando rapidamente o "ESTILO DE GRÁFICO" (por exemplo: incluindo os caracteres r, g ou b o gráfico fica com cor vermelha, verde ou azul; - indica uma linha sólida e -- uma linha tracejada; o, s e ^ fazem marcadores em cima de cada ponto com formato de bolinha, quadrado ou triângulo). 
+
+#O parâmetro formato é OPCIONAL, e por padrão o gráfico é feito com uma linha sólida ligando os pontos especificados. É possível passar parâmetros adicionais; veja a documentação para uma discussão detalhada.
+
+
+#Por exemplo, vamos graficar as funções:
+# y = x
+# y = x²
+# y = x³
+
+#O primeiro passo é gerar os valores de x que serão usados; aqui, usamos a função "linspace" do numpy. Em seguida graficamos as funções com a função plot e executamos o comando plt.show() para mostrar o gráfico na tela:
+x = np.linspace(0, 2, 21) # 21 pontos no intervalo [0, 2]
+plt.plot(x, x)            # função y = x
+
+plt.plot(x, x**2)         # função y = x^2
+
+plt.plot(x, x**3)         # função y = x^3
+
+plt.show() #Comando necessário para poder printar o gráfico
+
+
+# -----------------------------------FORMATO DA CURVA--------------------------------------------------------
+
+x = np.linspace(0, 2, 21)
+plt.plot(x, x, 'b--')   # linha azul (b) 
+                        # tracejada (--)
+plt.plot(x, x**2, 'go--') # verdes (g)
+                        # bolinhas (o) 
+                        # linhas tracejadas
+plt.plot(x, x**3, 'r^') # vermelhos (r)
+                        # nenhuma linha 
+                        # triângulos (^)
+plt.show()
+
+
+
+#------------------------------------DETALHES-----------------------------------------------------------------------------------------------------------
+
+
+# LEGENDAS (parâmetro label, função legend) para cada curva
+# TÍTULO no gráfico (title)
+# DESCRIÇÕES nos eixos coordenados (xlabel e ylabel) 
+# GRADE para facilitar a leitura do gráfico (grid):
+
+
+x = np.linspace(0, 2, 21)
+plt.plot(x, x, 'b--', label='linear')        # linear é a legenda da curva
+
+plt.plot(x, x**2, 'go', label='quadrático')  # quadrático é a legenda da curva
+
+plt.plot(x, x**3, 'r^', label='cúbico')      # cúbico é a legenda da curva
+
+plt.xlabel('teste x')                        # 'teste x' é o nome dado ao eixo x
+
+plt.ylabel('teste y')                        # 'teste y' é o nome dado ao eixo y
+
+plt.title('Gráfico simples')                 # 'Gráfico simples' é o nome do gráfico
+
+plt.legend()#comando que habilita as legendas do gráfico
+
+plt.grid()  #Adiciona um quadriculado ao gráfico
+
+plt.show()
