9b5bf34cbfb1a321ec7eed7945f3f438cfc23283
LordFabinho <145404540+LordFabinho@users.noreply.github.com>
2023-10-30 20:31:58 -0300

    Create POO.py

---
+++ POO.py
@@ -0,0 +1,17 @@
+#Exercício do slide de POO de Sergio
+
+class Aluno:
+    def __init__ ():
+        self.cpf = cpf
+        self.nome = nome
+        self.nota1 = nota1 
+        self.nota2 = nota2
+        self.nota3 = nota3
+    def inicializarNota(self, nota, numeroProva)
+        self.nota1 = 
+        self.nota2 = 
+        self.nota3 = 
+    def verificarSituacaoMedia(nota, numeroProva)
+        nota1 = 
+        nota2 = 
+        nota3 = 
