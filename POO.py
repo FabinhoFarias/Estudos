2a548bdddc10a2fc687e2244ac69ebe20f64d8d6
LordFabinho <145404540+LordFabinho@users.noreply.github.com>
2023-10-31 10:02:07 -0300

    Update POO.py

--- POO.py
+++ POO.py
@@ -1,12 +1,42 @@
+# --------------------------------------------------POO com python-------------------------------------------------------------------------
+
+#Objeto = Classe
+
+#O objeto tem métodos (ações) e atributos (informações)
+
+#Quando a classe é construida, as funções internas a essa classe são métodos e as informações dessas funções são os atributos.
+
+#Ex: O objeto "Pessoa" consegue fala, andar, pular, dormir... Essas ações seriam os métodos dentro da classe. 
+#    "Pessoa" também tem idade, cpf, nome, aniversário... Esses seriam os atributos da classe "Pessoa" 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Exercício do slide de POO de Sergio
 
 class Aluno:
-    def __init__ ():
-        self.cpf = cpf
-        self.nome = nome
-        self.nota1 = nota1 
-        self.nota2 = nota2
-        self.nota3 = nota3
+    def __init__ (self):
+        self.cpf = 0
+        self.nome = ''
+        self.nota1 = 0 
+        self.nota2 = 0
+        self.nota3 = 0
+        """
     def inicializarNota(self, nota, numeroProva)
         self.nota1 = 
         self.nota2 = 
@@ -15,3 +45,4 @@ class Aluno:
         nota1 = 
         nota2 = 
         nota3 = 
+        """
