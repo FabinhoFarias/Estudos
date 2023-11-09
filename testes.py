32541eeb58ec8effd96a4a038a60f8f3f3eabec5
LordFabinho <145404540+LordFabinho@users.noreply.github.com>
2023-11-09 18:14:56 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,14 +1,27 @@
 import numpy as np
 
-"""Q1 - Construa um algoritmo que efetue e apresente o resultado da soma entre duas matrizes 3 x 5. Inicialize a matriz com valores quaisquer e imprima o resultado na tela."""
 def questao_1_matrizes():
-    # CONDIÇÃO: As matrizes devem ser n x k
-    # Dada as matrizes A + B = C, A[linha][coluna] + B[linha][coluna] = C[linha][coluna]
-    matriz_1 = np.ones((3 ,5), dtype=int)
-    print(f'Matriz 1: \n{matriz_1}')
-    matriz_2 = np.ones((3 ,5), dtype=int)
-    print(f'Matriz 2: \n{matriz_2}')
+    matriz = np.ones((3 ,3), dtype=int)
+    numero_de_lihas = 0
+    numero_de_colunas = 0
+    for linha in matriz:
+        numero_de_lihas += 1
+        for elemento in linha:
+            numero_de_colunas += 1
+            elemento = int(input(f'A{numero_de_lihas}{numero_de_colunas}: '))
+        numero_de_colunas = 0
+        
+        
+    K = int(input('Constante: '))
+    #dimensoes = matriz_1.shape
+    print(f'Matriz: \n{matriz}')
+    #matriz_reposta = np.empty((dimensoes[0], dimensoes[1]), dtype=float)
     #RESOLUÇÃO: faz a soma escalar das linhas da matriz
-    #for i in matriz_1:
-
+    for linha in matriz:
+        numero_de_lihas += 1
+        for elemento in linha:
+            numero_de_colunas += 1
+            elemento = int(input(f'A{numero_de_lihas}{numero_de_colunas}: '))
+        numero_de_colunas = 0
+    #print(matriz_reposta)
 questao_1_matrizes()
