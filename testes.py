42bc38194e12d74e827dd7f00b23818d96ab092e
LordFabinho <145404540+LordFabinho@users.noreply.github.com>
2023-11-10 00:50:45 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,27 +1,12 @@
 import numpy as np
 
-def questao_1_matrizes():
-    matriz = np.ones((3 ,3), dtype=int)
-    numero_de_lihas = 0
+def questão_3_matrizes():
+    numero_de_linhas = 0
     numero_de_colunas = 0
-    for linha in matriz:
-        numero_de_lihas += 1
-        for elemento in linha:
-            numero_de_colunas += 1
-            elemento = int(input(f'A{numero_de_lihas}{numero_de_colunas}: '))
-        numero_de_colunas = 0
-        
-        
-    K = int(input('Constante: '))
-    #dimensoes = matriz_1.shape
-    print(f'Matriz: \n{matriz}')
-    #matriz_reposta = np.empty((dimensoes[0], dimensoes[1]), dtype=float)
-    #RESOLUÇÃO: faz a soma escalar das linhas da matriz
-    for linha in matriz:
-        numero_de_lihas += 1
-        for elemento in linha:
-            numero_de_colunas += 1
-            elemento = int(input(f'A{numero_de_lihas}{numero_de_colunas}: '))
-        numero_de_colunas = 0
-    #print(matriz_reposta)
-questao_1_matrizes()
+    matriz = np.random.randint(1, 401, size=(20,20))
+    elemento_busca = float(input(f'Float: '))
+    matriz_ordenada = np.sort(matriz)
+    print(f'{matriz}\n\n{matriz_ordenada}')
+    indices = np.where(matriz_ordenada == elemento_busca)
+    print(indices)
+questão_3_matrizes()
