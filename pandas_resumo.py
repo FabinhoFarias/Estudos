57ef94c5c6aa3d51efd448172ad7807f36a930d2
LordFabinho <145404540+LordFabinho@users.noreply.github.com>
2023-11-17 14:47:37 -0300

    Update pandas_resumo.py

--- pandas_resumo.py
+++ pandas_resumo.py
@@ -1,38 +1,43 @@
-import time
-import random as rd
-import numba
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# Usando barra dupla no caminho do arquivo
+caminho_do_arquivo = 'Python/Resultados_simulado.csv'
 
-stop = True
+# Ou usando barra invertida no estilo Unix
+# caminho_do_arquivo = 'C:/Users/FABINHO/Desktop/Códigos/Resultado_simulado.csv'
 
-while stop:
-    entrada = int(input("Digite n: "))
-    
-    def tamanho_do_array(n):
-        # Tamanho de n da lista
-        tempo_inicial = time.time()
-        lista = [rd.randint(1, 100) for _ in range(n)]
-        tempo_final = time.time()
-        tempo_de_resposta = tempo_final - tempo_inicial
-        print(f'Duração random: {tempo_de_resposta}')
-        return lista
+dataFrame = pd.read_csv(caminho_do_arquivo)
 
-    def soma(lista):
-        soma = 0
-        for i in lista:
-            soma += i
-        return soma
+#print(dataFrame)
 
-    lista = tamanho_do_array(entrada)
+coluna_nomes = np.array(dataFrame['Nome'])
+coluna_linguagens = np.array(dataFrame['Linguagens '])
+coluna_mat = np.array(dataFrame['Matemática'])
+coluna_nt = np.array(dataFrame['Natureza'])
+coluna_human = np.array(dataFrame['Humanas'])
 
-    tempo_inicial = time.time()
-    resultado_soma = soma(lista)
-    tempo_final = time.time()
-    tempo_de_resposta = tempo_final - tempo_inicial
 
-    print()
-    print(f'Soma: {resultado_soma}')
-    print(f'Duração do for: {tempo_de_resposta}')
 
-    resposta = input("Deseja continuar? (S/N) ").strip().lower()
-    if resposta != "s":
-        stop = False
+
+
+soma_mat = coluna_mat.sum()/len(coluna_mat)
+#print(soma_mat)
+
+
+
+
+
+
+def notas (array_nomes, array_notas):
+    array_nomes = np.array(array_nomes)
+    metade_Nomes = array_nomes[0:int(len(array_nomes)/4)]
+    metade_Notas = array_notas[0:int(len(array_notas)/4)]
+    plt.xticks(range(len(metade_Nomes)), metade_Nomes, rotation=45, ha='right')
+
+    array_notas = np.array(array_notas)
+    plt.plot(metade_Nomes, metade_Notas, 'r^')
+    plt.show()
+#print(coluna_mat)
+
+notas(coluna_nomes, coluna_mat)
