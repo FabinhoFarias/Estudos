a5775beb8f3bd48b4dc98a94adc2c0fca50e82b4
LordFabinho <145404540+LordFabinho@users.noreply.github.com>
2023-11-22 12:39:06 -0300

    Update fundamentos_aleatorios.py

--- fundamentos_aleatorios.py
+++ fundamentos_aleatorios.py
@@ -27,43 +27,11 @@ for exemplo in exemplos:
     resultado = encontrar_elemento_unico(exemplo)
     print(f"A lista {exemplo} retorna o elemento único: {resultado}")
 
-
-#
-import time
-import random as rd
-import numba
-
-stop = True
-
-while stop:
-    entrada = int(input("Digite n: "))
-    
-    def tamanho_do_array(n):
-        # Tamanho de n da lista
-        tempo_inicial = time.time()
-        lista = [rd.randint(1, 100) for _ in range(n)]
-        tempo_final = time.time()
-        tempo_de_resposta = tempo_final - tempo_inicial
-        print(f'Duração random: {tempo_de_resposta}')
-        return lista
-
-    def soma(lista):
-        soma = 0
-        for i in lista:
-            soma += i
-        return soma
-
-    lista = tamanho_do_array(entrada)
-
-    tempo_inicial = time.time()
-    resultado_soma = soma(lista)
-    tempo_final = time.time()
-    tempo_de_resposta = tempo_final - tempo_inicial
-
-    print()
-    print(f'Soma: {resultado_soma}')
-    print(f'Duração do for: {tempo_de_resposta}')
-
-    resposta = input("Deseja continuar? (S/N) ").strip().lower()
-    if resposta != "s":
-        stop = False
+def combinação_de_ordenadas():
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    lista = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n] #nos ranges entra x+1 por conta da exclusão do ultimo n
+    print(lista)
+#combinação_de_ordenadas()
