0a40577669d5a9be67b86515c066bab6c37c7259
LordFabinho <145404540+LordFabinho@users.noreply.github.com>
2023-11-27 19:10:56 -0300

    Update POO.py

--- POO.py
+++ POO.py
@@ -10,7 +10,7 @@
 #O objeto tem métodos (ações) e atributos (informações)
 
 #Quando a classe é construida, as funções internas a essa classe são métodos e as informações dessas funções são os atributos.
-
+ 
 #Ex: O objeto "Pessoa" consegue fala, andar, pular, dormir... Essas ações seriam os métodos dentro da classe. 
 #    "Pessoa" também tem idade, cpf, nome, aniversário... Esses seriam os atributos da classe "Pessoa". Atribultos também podem ser variáveis dentro do código.
 
