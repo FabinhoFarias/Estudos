99bc4e25b0211bf28750064c9c58109b1e7818aa
Fábio Filho <145404540+LordFabinho@users.noreply.github.com>
2023-12-12 23:08:40 -0300

    Update Automação.py

--- Automação.py
+++ Automação.py
@@ -2,7 +2,7 @@ import pandas as pd #iniciar - módulos usados
 resultados = pd.read_csv('Resultados simulado.csv') # ( varivel pd.read_(tipo do arquivo(ex:_csv))('arquivo com '.csv' no final)) #essa função read vai ler o arquivo e o transformar em tabelas 
 print(resultados)
 #abrir site para coleta de dados
-#integrar com o arquivo exel
+#integrar com o arquivo exel 
 #copiar questoes
 #colar questoes na planilha
 #copiar gabarito
